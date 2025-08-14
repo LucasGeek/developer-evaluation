83768c57bf596379599df2be0c989e4c6339f414
Lucas Albuquerque <lucas.albuquerque.gk@gmail.com>
2025-08-13 23:33:13 -0400

    chore: update build artifacts for domain model

--- src/Ambev.DeveloperEvaluation.Application/obj/Debug/net8.0/Ambev.DeveloperEvaluation.Application.AssemblyInfo.cs
+++ src/Ambev.DeveloperEvaluation.Application/obj/Debug/net8.0/Ambev.DeveloperEvaluation.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ambev.DeveloperEvaluation.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0947854bf6e6921eef5321d91290fcecb111d760")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af087cf5087bac2cbe77883e0dd0373534c5ca74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ambev.DeveloperEvaluation.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ambev.DeveloperEvaluation.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
